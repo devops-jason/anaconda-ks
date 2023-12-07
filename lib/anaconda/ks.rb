22b1cce78163103d9a640c4fe1a1ab1353cbbb2d
Jason Miller <jasonlmiller@jlmillerelectric.com>
2023-12-07 05:52:29 -0800

    first commit

---
+++ lib/anaconda/ks.rb
@@ -0,0 +1,7 @@
+require "anaconda/ks/version"
+
+module Anaconda
+  module Ks
+    # Your code goes here...
+  end
+end
---
+++ lib/anaconda/ks/version.rb
@@ -0,0 +1,5 @@
+module Anaconda
+  module Ks
+    VERSION = "0.1.0"
+  end
+end
